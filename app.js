8c3b22a1840c75ca9065603e683e12fc504cac9a
xaviermatroski <141320984+xaviermatroski@users.noreply.github.com>
2024-07-08 18:13:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -249,6 +249,6 @@ app.get("/users/:user", function(req, res){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started");
 });
